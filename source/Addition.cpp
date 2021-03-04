e7af743f83cd46bc8b9f51f7d9668f249ca77394
Rafael Fernandes <63118583+rafaelzolt@users.noreply.github.com>
2021-03-04 09:03:53 +0100

    Changed demo code to take a random time to execute

--- source/Addition.cpp
+++ source/Addition.cpp
@@ -10,7 +10,16 @@ Addition::Addition(int id) : id_(id) {
 
 Addition::~Addition() {}
 
-int Addition::add(int a, int b) { return a + b; }
+  
+int Addition::add(int a, int b) { 
+  
+// wait random time
+  std::mt19937_64 eng{std::random_device{}()};  // or seed however you want
+  std::uniform_int_distribution<> dist{10, 100};
+  std::this_thread::sleep_for(std::chrono::milliseconds{dist(eng)});
+
+  return a + b;
+}
 int Addition::addID(int val) { return add(val,id_); }
 int Addition::addOne(int val) { return add(val,1); }
 int Addition::addTwo(int val) { return add(val,2); }
